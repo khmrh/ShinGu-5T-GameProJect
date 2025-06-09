f54824cbb9f1e50d67da862cab8c2973bcb6c20f
kimrh <kiedoda2@gmail.com>
2025-06-09 12:07:20 +0900

    [Feat] 상점칸 구현 및 슬롯클릭시 구매활성완성,임시설정버튼

--- Assets/Script/ShopScripts/ShopSlot.cs
+++ Assets/Script/ShopScripts/ShopSlot.cs
@@ -9,7 +9,6 @@ public class ShopSlot : MonoBehaviour
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI descriptionText;
     public TextMeshProUGUI priceText;
-    public Button purchaseButton;
 
     private PassiveAbility ability;
 
@@ -21,13 +20,22 @@ public class ShopSlot : MonoBehaviour
         descriptionText.text = $"{ability.description}\n▶ 누적 효과: {ability.GetTotalValue()}";
         priceText.text = "가격: 0";
 
-        purchaseButton.onClick.RemoveAllListeners();
-        purchaseButton.onClick.AddListener(OnClickPurchase);
+        // 슬롯 전체에 붙은 Button 컴포넌트에 클릭 이벤트 연결
+        var btn = GetComponent<Button>();
+        if (btn != null)
+        {
+            btn.onClick.RemoveAllListeners();
+            btn.onClick.AddListener(OnClickPurchase);
+        }
+        else
+        {
+            Debug.LogWarning("ShopSlot에 Button 컴포넌트가 없습니다!");
+        }
     }
 
     public void OnClickPurchase()
     {
         PassiveManager.Instance.Purchase(ability);
-        SetUp(ability);
+        SetUp(ability); // UI 갱신
     }
 }
